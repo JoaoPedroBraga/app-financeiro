8dadd00db6120eaf0e70791e7f62b553c57970a7
Joao Pedro <cls>
2024-03-27 13:10:42 -0300

    Adicionando Axios e começando integração do login com o back

--- src/components/LoginForm/index.jsx
+++ src/components/LoginForm/index.jsx
@@ -1,19 +1,35 @@
 'use client'
-
-// import Button from "@mui/material/Button";
+import { useState } from 'react';
+import axios from 'axios';
 import * as S from './style';
 
 export const LoginForm = () => {
-    const onSubmit = (event) => {
-        event.preventDefault();
-        console.log('Formulario Enviado');
-    };
+    const [email, setEmail] = useState();
+    const [password, setPassword] = useState();
 
+    const onChangeValue = (e) => {
+        const {name, value} = e.target
+        if (name === 'email') setEmail(value)
+        if (name === 'password') setPassword(value)
+    }
+
+    const onSubmit = async (event) => {
+        event.preventDefault();
+        try {
+          const response =  await axios.post('http://localhost:8080/auth/login', {email, password: password})
+          localStorage.setItem('token', response.data.data.token)  
+          console.log('res', response);
+        }
+        catch (err) {
+            console.log('error', err);
+        }
+
+    }
     return (
         <form onClick={ onSubmit }>
             <S.H1>Login</S.H1>
-            <S.TextField id="email" label="Email" variant="outlined" />
-            <S.TextField id="password" type='password' label="password" variant="outlined" />
+            <S.TextField name='email' onChange={onChangeValue} label="Email" variant="outlined" />
+            <S.TextField name='password'onChange={onChangeValue} type='password' label="password" variant="outlined" />
             <S.Button variant="outlined" color="success">Entrar</S.Button>
         </form>
     )
